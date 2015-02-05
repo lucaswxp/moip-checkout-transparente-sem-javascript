a555f29d650c83fa587beac79223c943adcfa182
Lucas Pelegrino <lucaswxp@lucaswxp-macmini.local>
2015-02-05 01:55:55 -0300

    first commit

---
+++ MoipCheckout.php
@@ -0,0 +1,110 @@
+<?php
+/**
+  * Depende das funções: json_encode, json_decode & curl
+  *
+  * Esse script deve trabalhar em conjunto com o PHP SDK oficial da moip: https://github.com/moiplabs/moip-php.
+  *
+  * Utilize a biblioteca oficial da Moip para pegar o token da transação e em seguida
+  * utilize essa classe para finalizar a transação SEM a necessidade de utilizar Javascript,
+  * na etapa final do proceso de checkout.
+  *
+  * Todas as informações que você passaria para a api JS você pode passar nesta versão PHP através do método ->set()
+  * e em seguida efetuar a requisição com ->getAnswer().
+  *
+  *
+  * Como usar:
+
+    $checkout = new MoipCheckout($meuToken);
+    $checkout->sandbox = true;
+    $checkout->set(array(
+      'Forma' => 'CartaoCredito',
+      'Parcelas' => 1,
+      'Instituicao' => 'Visa',
+      'CartaoCredito' => array(
+        'Numero' => '4073020000000002',
+        'Expiracao' => '12/15',
+        'CodigoSeguranca' => '123',
+        'Portador' => array(
+          'Nome' => 'Nome Sobrenome',
+          'DataNascimento' => '30/12/1987',
+           'Telefone' => '(11) 3165-6584',
+           'Identidade' => '222.222.222-22' // CPF!
+         )
+      )
+    ));
+
+    $data = $checkout->getAnswer();
+
+    if($data->StatusPagamento == 'Sucesso'){
+      echo 'Pagamento processado';
+    }else{
+      echo 'Algo deu errado: ';
+      exit($data['Mensagem'])
+    }
+
+  */
+
+class MoipCheckout {
+  public $sandbox;
+
+  private $token;
+
+  private $data = array();
+
+  const SANDBOX_ENDPOINT = 'https://desenvolvedor.moip.com.br/sandbox';
+  const PRODUCTION_ENDPOINT = 'https://www.moip.com.br';
+
+  public function __construct($token, $sandbox = false){
+    $this->token = $token;
+    $this->sandbox = $sandbox;
+  }
+
+  public function set(array $data){
+    $this->data = $data;
+  }
+
+  public function getAnswer(){
+    $curl = $this->getCurl();
+    $response = curl_exec($curl);
+    $response = json_decode(substr($response, 1, -1)); // substr remove parenteses
+
+    if($response->StatusPagamento === 'Sucesso'){
+      $response->url = $this->resolveUrl('/Instrucao.do?token=' . $this->token);
+    }
+
+    return $response;
+  }
+
+  public function getCurl(){
+    $qs = $this->getQuery();
+    $url = $this->resolveUrl('/rest/pagamento?' . http_build_query($qs));
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    return $ch;
+  }
+
+  public function getQuery(){
+    return array(
+      'callback' => '',
+      'pagamentoWidget' => json_encode(array(
+        'pagamentoWidget' => array(
+          'token' => $this->token,
+          'dadosPagamento' => $this->data,
+          'referer' => $this->getReferer()
+        )
+      ))
+    );
+  }
+
+  private function resolveUrl($url){
+    return ($this->sandbox ? self::SANDBOX_ENDPOINT : self::PRODUCTION_ENDPOINT) . $url;
+  }
+
+  private function getReferer(){
+    if($_SERVER['HTTP_REFERER']){
+      return $_SERVER['HTTP_REFERER'];
+    }else{
+      return ((stripos($_SERVER['SERVER_PROTOCOL'],'https') === true ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,98 @@
+Moip sem javascript
+===
+
+Essa classe foi criada para eliminar a necessidade que a Moip impõe de se utilizar
+a API javascript deles para finalizar uma transação.
+
+Por que eles mesmos não tem uma API completa? Não faço ideia! Mas isso não vai nos impedir ;)
+
+Esse script deve trabalhar em conjunto com o PHP SDK oficial da moip: https://github.com/moiplabs/moip-php
+
+Como funciona
+===
+É bem simples. Tudo o que a API Javascript da Moip faz é validar os dados que
+você informa na função "MoipWidget" e então envia as informações para o servidor
+através de uma requisição HTTP (ajax).
+
+Então tudo que temos que fazer é uma requisição HTTP para o servidor da Moip que tudo funcionará!
+Por isso será necessário que você tenha o curl instalado para que tudo funcione.
+
+Diferenças
+===
+A única diferença entre a API javascript e este repositório é que a versão Javascript
+irá VALIDAR seus dados antes de enviar para o servidor deles (cpf, datas, etc). Este comportamento
+NÃO foi implementado nessa classe, então tenha certeza de enviar dados validados
+para o objeto.
+
+Se por acaso você enviar informações erradas para o servidor, nada de mais irá acontecer, você
+ainda receberá um objeto com uma mensagem de erro na resposta, mas a mensagem não será
+tão clara quanto a versão javascript.
+
+Resumindo... tenha certeza de enviar dados válidos.
+
+O código
+===
+
+Sem mais papo, aqui estão dois exemplos de código para cartão de crédito e outro para boleto:
+
+Exemplo com Cartão de Crédito
+-------
+```php
+require 'MoipCheckout.php';
+
+// o $token você pega com a SDK que a própria Moip disponibiliza: https://github.com/moiplabs/moip-php
+
+$checkout = new MoipCheckout($token);
+$checkout->sandbox = true; // mude para false quando entrar em produção
+$checkout->set(array(
+  'Forma' => 'CartaoCredito',
+  'Parcelas' => 1,
+  'Instituicao' => 'Visa',
+  'CartaoCredito' => array(
+    'Numero' => '4073020000000002',
+    'Expiracao' => '12/15',
+    'CodigoSeguranca' => '123',
+    'Portador' => array(
+      'Nome' => 'Nome Sobrenome',
+      'DataNascimento' => '30/12/1987',
+       'Telefone' => '(11) 3165-6584',
+       'Identidade' => '222.222.222-22' // CPF!
+     )
+  )
+));
+
+$data = $checkout->getAnswer(); // faz a requisição http
+
+if($data->StatusPagamento == 'Sucesso'){
+  echo 'Pagamento processado';
+}else{
+  echo 'Algo deu errado: ';
+  exit($data['Mensagem'])
+}
+```
+
+
+Exemplo com Boleto Bancário
+-------
+```php
+require 'MoipCheckout.php';
+
+// o $token você pega com a SDK que a própria Moip disponibiliza: https://github.com/moiplabs/moip-php
+
+$checkout = new MoipCheckout($token);
+$checkout->sandbox = true; // mude para false quando entrar em produção
+$checkout->set(array(
+  'Forma' => 'BoletoBancario',
+  'Instituicao' => 'BancoDoBrasil'
+));
+
+$data = $checkout->getAnswer(); // faz a requisição http
+
+if($data->StatusPagamento == 'Sucesso'){
+  // redireciona o usuário para a URL boleto
+  header('Location: ' . $data->url);
+}else{
+  echo 'Algo deu errado: ';
+  exit($data['Mensagem'])
+}
+```
